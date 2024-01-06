0e9f227d79f24c3de64c387315a77567522d5193
Timothy C. Sweeney-Fanelli <tim@zerobytellc.com>
2024-01-06 09:09:49 -0500

    Pointing to modprobed.db

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git clean -fdx
-env _microarchitecture=14 _localmodcfg=y _compiler_flags="-j 32" makepkg -si --noconfirm
+env LSMOD=/home/timcsf/.config/modprobed.db _microarchitecture=14 _localmodcfg=y _compiler_flags="-j 32" makepkg -si --noconfirm
