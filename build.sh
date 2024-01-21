9d8574506ea13c5711d58d1b5201a4fc51eeb526
Timothy C. Sweeney-Fanelli <tim@zerobytellc.com>
2024-01-21 11:56:24 -0500

    Using -fcsi instead of doing a git clean

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-git clean -fdx
-env LSMOD=/home/timcsf/.config/modprobed.db _microarchitecture=14 _localmodcfg=y _compiler_flags="-j 32" makepkg -si --noconfirm
+#git clean -fdx
+env _microarchitecture=14 _compiler_flags="-j 32" makepkg -fcsi --noconfirm
+#env LSMOD=/home/timcsf/.config/modprobed.db _microarchitecture=14 _localmodcfg=y _compiler_flags="-j 32" makepkg -si --noconfirm
