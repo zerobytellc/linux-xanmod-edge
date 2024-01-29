2d8a8add80238e8c9428715c3bc4518cf9017575
Timothy C. Sweeney-Fanelli <tim@zerobytellc.com>
2024-01-29 09:35:10 -0500

    Switching back to localmdocfg

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #git clean -fdx
-env _microarchitecture=14 _compiler_flags="-j 32" makepkg -fcsi --noconfirm
-#env LSMOD=/home/timcsf/.config/modprobed.db _microarchitecture=14 _localmodcfg=y _compiler_flags="-j 32" makepkg -si --noconfirm
+#env _microarchitecture=14 _compiler_flags="-j 32" makepkg -fcsi --noconfirm
+env LSMOD=/home/timcsf/.config/modprobed.db _microarchitecture=14 _localmodcfg=y _compiler_flags="-j 32" makepkg -si --noconfirm
